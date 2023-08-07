54a2995f3a8fa33e28011e690fdc906b4b50cb4b
raquel king <kingraq1007@gmail.com>
2023-08-07 10:55:15 -0500

    Week 3 assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,19 +15,76 @@
 // a) Create a test with expect statements for each of the variables provided.
 
 const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("fibLength", () => {
+  it("returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+    expect(fibLength()).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibLength()).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// b) Create the function that makes the test pass.
+// // Expected output: [1, 1, 2, 3, 5, 8]
 
-// Pseudo code:
+// //const fibLength2 = 10
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+// // b) Create the function that makes the test pass.
+
+// // Pseudo code: 
+//  //Pseudo Code: 
+//   //Start by defining a function that takes an number as a parameter
+//   const fibLength = (num) => {
+//     if (num < 3) {
+//         return ("Number must be greater than 2")
+//     }
+
+  
+//   //Create an if statement to see if conditions were met (number is greater than 2)
+//   //The first two numbers of the expected output are 1,1 , initialize and declare a new array, having 1, 1 as values
+//   const fibLength1 = [1,1]
+
+//   //Create a for loop that starting at the index of 2 because arrays are 0 index and index 0 and 1 are already in the array.
+//   //The array should check if the number and length are greater than 2
+//   //Inside of the loop, we decalre another variable pushNum to calculate the next number. It will added to the sequence of numbers by combining the last two numbers in the array
+//   //Using the .push method, we add the numbers to the end of the pushNum array
+
+//   for (let i = 2; i< num; i++) {
+//     const pushNum = fibInit[i-1] + fibInit[1-2]
+//      fibLength.push(fibLength1) 
+     
+    
+//      }
+//      return fibLength1
+    
+//     }
+
+  //return fiblength
+
+ //Every test that I have done to make the test pass has failed, at first it was that the return was not in the function, and then Test suite failed to run, 
+    //- Expected  - 4
+   // + Received  + 0
+   //I think what is confusing me the most is knowing which constant goes where. I  moved them around quite a bit trying to figure out how to make the test pass
+
+  
+  
+
+
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("sortedLeast", () => {
+  it("returns an array of the values sorted from least to greatest", () => {
+    expect(sortedLeast()).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(sortedLeast()).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -38,36 +95,75 @@ const studyMinutesWeek1 = {
   friday: 15,
   saturday: 60
 }
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+// // //Expected output: [15, 15, 20, 30, 30, 60, 90]
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
+// // const studyMinutesWeek2 = {
+// //   sunday: 100,
+// //   monday: 10,
+// //   tuesday: 45,
+// //   wednesday: 60,
+// //   thursday: 20,
+// //   friday: 15,
+// //   saturday: 65
+// // }
+// // Expected output: [10, 15, 20, 45, 60, 65, 100]
+
+
+// // b) Create the function that makes the test pass.
+//Create a function and declare a variable that takes in an object
+
+const sortedLeast = (object) => {
+  const studyMinutesWeek= (studyMinutesWeek1.value)
+  studyMinutesWeek1.sort((a,b)=> a-b)
+  return sortedLeast
 }
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+// The value is required so we create a variable to store the objects value
+//usingh .sort method, we are able to sort the arrValue numbers from least to greatest.
 
-// b) Create the function that makes the test pass.
+//ERRORS:
+//TypeError: Cannot read properties of undefined (reading 'value')
 
-// Pseudo code:
+// // Pseudo code:
 
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+// // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+describe("inOrder", () => {
+  it("returns an array of the accumulating sum", () => {
+    expect(inOrder()).toEqual([100, 83, 60, 51])
+    expect(inOrder()).toEqual([250, 161, 261, 165])
+    expect(inOrder()).toEqual([])
+  })
+})
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
 
-const accountTransactions3 = []
-// Expected output: []
+// // const accountTransactions1 = [100, -17, -23, -9]
+// // // Expected output: [100, 83, 60, 51]
 
-// b) Create the function that makes the test pass.
+// // const accountTransactions2 = [250, -89, 100, -96]
+// // // Expected output: [250, 161, 261, 165]
 
-// Pseudo code:
+// // const accountTransactions3 = []
+// // Expected output: []
+
+// // b) Create the function that makes the test pass.
+// //Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+
+
+// // Pseudo code:
+// //Input: Array of numbers
+// //Output: Array of numbers
+// //Create  function that takes in array of numbers as the parameter
+// //.map() method to iterate over array and adds accumulating sum using appropriate operators
+// Values will need to be stored, create a new variable
+
+
+const inOrder = (arr) => {
+const addSum = accountTransactions1.map()
+//I was unsure of how to get the accumulating sum. I know that a for loop would be able to loops around until the condition of adding is met but not sure of the syntax. 
+//for (let i = 1; i + num) // or something similar is what I was thinking but my tests failed. I want to add [100, -17, -23, -9] for an output of [100, 83, 60, 51].
+
+//Each value 0 index + 1 index... and getting it to appear is what is most confusing.
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,46 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is the language used when working in React and combines JavaScript and HTML. One syntax difference between HTML and JSX is that JSX uses curly brackets while HTML does not.
 
-Researched answer:
+Researched answer: JSX is a combination of Javascript and HTML. The difference between the two is that HSX requires a parent element to function properly, while HTML does not.
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager and the files modified are the ones that hold code
 
-Researched answer:
+Researched answer: Yarn is a package manager for code, so that it can be shared with other developers. A package in yarn allows for the sharing of code. The files modified are the yarn.lock files and node_modules directory
 
-3. What is a React component?
+3. What is a React component? 
+Your answer: A React component is method that allows the user to add certain features to an App
 
-Your answer:
-
-Researched answer:
-
+Researched answer: When using React to create an App, you would use components. Components are  created and will be what the user sees, for example, a button or text input box
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values will be the value that the user inputs while props are what gets passed through to change the properties of a component
 
-Researched answer:
+Researched answer:  Props will get passed through the component, while state is managed within the component 
 
 5. What is the DOM?
 
-Your answer:
+Your answer: Document Object Model and it is what is displayed for the user.
 
-Researched answer:
+Researched answer: The DOM is responsible for displaying the content, structure and style for users
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: Div tags serve as a container and are used to divide a page and information on that page. I am not too familiar with span tags
 
-Researched answer:
+Researched answer: Div tags blocks of a portion of the webpage and span is used inline
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Normally computer programming will consist of logic and functions but with OOP, software is designed around objects and data
 
-2. Ruby:
+2. Ruby: Ruby is a programming language that uses of objects. It is a high level programming language and used for general purposes
 
-3. Implicit return:
+3. Implicit return: When a function has a returned value without using a return 
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are little bits of code functionality that can be re-used
 
-5. Ruby hashes:
+5. Ruby hashes: Hashes are a data structure and allow you to store data in key-value pairs
